58d391fc55436606ba1e0dac5f47bc1b825fcad6
butterincup <143638043+butterincup@users.noreply.github.com>
2024-02-09 15:56:50 -0700

    Add files via upload

---
+++ EVA1_2_identificadores/src/eva1_2_identificadores/EVA1_2_identificadores.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_identificadores;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_2_identificadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES: 
+        //Tipo de datos + nombre del identificador:
+        //short, int, byte, long --> enteros
+        //float, double --> numeros con decimal
+        //char--> character --> caracteres
+        //boolean --> verdadero o falso
+        //String --> cadenas de texto
+        // si no se declara una variable, no se puede usar;
+    int edad; // declaracion de una variable numero entero;
+    String nombre; //declaracion de una variable texto;
+    double salario; // declaracion de una variable de numero decimal;
+    
+    //Asignacion de valores;
+    //Identificador = Valor;
+    // se cloloca el valor 50 dentro de la variable edad;
+    edad = 50;
+    nombre= "paco";
+    salario=123.3;
+    // como los usamos:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
