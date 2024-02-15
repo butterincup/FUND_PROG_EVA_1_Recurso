7c2d7cca46bef5c42b0c491b1d046e8dcd0de607
butterincup <143638043+butterincup@users.noreply.github.com>
2024-02-15 15:44:37 -0700

    Add files via upload

---
+++ EVA1_4_asignacion/src/eva1_4_asignacion/EVA1_4_asignacion.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_4_asignacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;
+        String nombre;
+        String carrera;
+        edad=19;
+        nombre="luis raul tarres valles";
+        carrera="sistemas computacionales";
+        System.out.println("tu edad es: "+ edad);
+        System.out.println("tu nombre es: "+ nombre);
+        System.out.println("tu carrera es: "+ carrera);
+                
+    }
+    
+}
